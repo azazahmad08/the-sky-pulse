3e9a964adefd7461b969636b5d380a8eb3725d44
azazahmad08 <engr.azaz08@gmail.com>
2023-09-19 21:43:28 +0600

    Before API Intregation

--- src/Components/WeatherApp/weatherApp.jsx
+++ src/Components/WeatherApp/weatherApp.jsx
@@ -11,15 +11,35 @@ import humidity_icon from '../Assets/humidity.png';
 
 
 export const WeatherApp = () => {
+    let api_key = "e474d44f4ecdb39274a72eabce5ecbeb";
   return (
     <div className='container'>
       <div className='top-bar'>
         <input type="text" className='cityInput' placeholder='Search '  />
         <div className='search-icon'>
-            <img src={search_icon} alt="" />
+            <img src={search_icon} alt="Search Icon" />
         </div>
       </div>
-
+      <div className="weather-image">
+        <img src={Cloud_icon} alt="Cloud Icon" />
+      </div>
+      <div className="weather-temp">24°c</div>
+      <div className="weather-location">Dhaka</div>
+      <div className="data-container">
+    <div className="element">
+            <img src={humidity_icon} alt="" className='icon' />
+            <div className="data">
+             <div className="humidity-percentent">64%</div>      
+            </div>    
+    </div>
+    <div className="element">
+            <img src={wind_icon} alt="" className='icon' />
+            <div className="data">
+                <div className="humidity-percent">18k/h</div>
+                <div className="text">Wind Speed</div>      
+            </div>
+    </div>
+    </div>
     </div>
   )
 }
